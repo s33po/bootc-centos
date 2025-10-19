59b8149561d3148d6552ccc3a6acb16fe38a34fd
s33po <seep0@pm.me>
2025-10-19 21:37:49 +0300

    feat: add basic set of fonts

--- build_files/20-desktop.sh
+++ build_files/20-desktop.sh
@@ -6,12 +6,22 @@ set -xeuo pipefail
 dnf -y copr enable @centoshyperscale/c10s-gnome-48
 dnf -y upgrade glib2
 
-# Install only specific langpacks for needed languages/locales etc. (edit for your own needs)
-# This makes final image smaller and saves some build time/bandwidth
+# Install only specific langpacks for needed languages/locales (edit for your own needs)
+# This saves +200 MB compared to installing the full 'glibc-all-langpacks'
 dnf -y install glibc-langpack-{en,fi}
+
+# Install small set of fonts for system UI, terminal, basic document and web compatibility
+# This saves +100 MB compared to installing the full "Fonts" group (you can install more fonts locally)
+dnf -y install --setopt=install_weak_deps=False \
+    google-noto-sans-fonts \
+    google-droid-sans-mono-fonts \
+    liberation-fonts \
+    dejavu-sans-fonts \
+    redhat-display-vf-fonts
 
 # Install "Workstation" without default fonts, browser and printing support (edit for your own needs)
 # Includes GNOME and a basic set of packages for a desktop system
+# Excluding "Printing client" (if you dont need it) saves about 100 MB
 dnf -y group install --setopt=install_weak_deps=False \
     "base-graphical" \
     "Common NetworkManager submodules" \
